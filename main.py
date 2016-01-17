89fcf556d6b6086434ea961c19b770ffd3fdc7be
Alex Flores Escarcega <alex.floresescar@gmail.com>
2016-01-17 15:54:31 -0600

    Add imports, logging configs, and authorship info

--- main.py
+++ main.py
@@ -9,12 +9,28 @@ The MIT License (MIT)
 (c) 2016
 """
 
+import pyautogui, logging, time
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s.%(msecs)03d: %(message)s', datefmt='%H:%M:%S')
+
+# logging.disable(logging.DEBUG) # uncomment to block debug log messages
+
+__author__ = "Alex Flores Escarcega"
+__copyright__ = "Copyright 2007, Alex Flores Escarcega"
+__credits__ = ["Alex Flores Escarcega"]
+__license__ = "MIT"
+__version__ = "1.0.1"
+__maintainer__ = "Alex Flores Escarcega"
+__email__ = "alex.floresescar@gmail.com"
+__status__ = "Development"
+ 
 def main():
     """
     No inputs
     No outputs
     Starts up the gameregionfinder
     """
+
 
 
 if __name__ == "__main__":
