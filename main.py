8716d50685a5ca38889a15d5458855342557413b
Alex Flores Escarcega <alex.floresescar@gmail.com>
2016-01-17 15:36:16 -0600

    Add shebang, docstring and main runner

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env
+
+"""
+This is the main file. The script finds the game window and sets up the 
+coordinates for each block.
+
+
+The MIT License (MIT) 
+(c) 2016
+"""
+
+if __name__ == "__main__":
+        main()
